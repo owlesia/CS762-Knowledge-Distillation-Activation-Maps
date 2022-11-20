108e3a61f60d584a03bf4efcb617d577af010f88
Nitin <nitinbtsaxena@gmail.com>
2022-11-20 20:06:26 +0000

    Transfer Learning Experiments

---
+++ KD_examples/Example3_Olesia_Quant/dataloader.py
@@ -0,0 +1,226 @@
+"""
+Create train, valid, test iterators.
+"""
+
+import torch
+import torchvision
+import numpy as np
+from torchvision import datasets
+from torchvision import transforms as T
+from torch.utils.data.sampler import SubsetRandomSampler
+import matplotlib.pyplot as plt
+
+
+mean=np.array([0.485, 0.456, 0.406]) 
+std=np.array([0.229, 0.224, 0.225])
+
+def get_train_valid_loader(data_dir,
+                           batch_size,
+                           augment,
+                           random_seed,
+                           valid_size=0.2,
+                           shuffle=True,
+                           show_sample=False,
+                           num_workers=1,
+                           pin_memory=True):
+    """
+    Utility function for loading and returning train and valid
+    multi-process iterators over the CIFAR-10 dataset. A sample
+    9x9 grid of the images can be optionally displayed.
+
+    If using CUDA, num_workers should be set to 1 and pin_memory to True.
+
+    Params
+    ------
+    - data_dir: path directory to the dataset.
+    - batch_size: how many samples per batch to load.
+    - augment: whether to apply the data augmentation scheme
+      mentioned in the paper. Only applied on the train split.
+    - random_seed: fix seed for reproducibility.
+    - valid_size: percentage split of the training set used for
+      the validation set. Should be a float in the range [0, 1].
+    - shuffle: whether to shuffle the train/validation indices.
+    - show_sample: plot 9x9 sample grid of the dataset.
+    - num_workers: number of subprocesses to use when loading the dataset.
+    - pin_memory: whether to copy tensors into CUDA pinned memory. Set it to
+      True if using GPU.
+
+    Returns
+    -------
+    - train_loader: training set iterator.
+    - valid_loader: validation set iterator.
+    """
+    error_msg = "[!] valid_size should be in the range [0, 1]."
+    assert ((valid_size >= 0) and (valid_size <= 1)), error_msg
+
+    normalize = T.Normalize(mean,std)
+
+    # define T
+    valid_transform = T.Compose([
+            T.Resize((256,256)),
+            T.CenterCrop((224,224)),
+            T.ToTensor(),
+            normalize,
+    ])
+    if augment:
+        train_transform = T.Compose([
+            T.Resize((256,256)),
+            T.RandomHorizontalFlip(),
+            T.RandomRotation(degrees=(0, 180)),
+            T.RandomPerspective(distortion_scale=0.6, p=1.0),
+            T.RandomInvert(), # Olesia
+            T.RandomAutocontrast(), # Olesia
+            T.ColorJitter(), # Olesia 2
+            T.RandomEqualize(), # Olesia 2
+            T.RandomCrop((224,224)),
+            T.ToTensor(),
+            normalize,
+        ])
+    else:
+        train_transform = T.Compose([
+            T.Resize((224,224)),
+            T.ToTensor(),
+            normalize,
+        ])
+
+    # load the dataset
+    train_dataset = datasets.OxfordIIITPet(
+        root=data_dir, split = 'trainval',
+        download=False, transform=train_transform,
+    )
+    
+    # Selecting classes "Bengal", "Chihuahua"
+    # train_idx = []
+    # for i in range(len(train_dataset)):
+    #     if train_dataset.__getitem__(i)[1] in [1, 2, 3]:
+    #         train_idx.append(i)
+    # train_dataset = torch.utils.data.Subset(train_dataset, train_idx)
+    
+    # train_dataset.labels = train_dataset.labels[idx]
+    # train_dataset.data = train_dataset.data[idx]
+    
+    # Olesia added to limit size of the datasets
+    #train_dataset = torch.utils.data.random_split(train_dataset, [1000, len(train_dataset)-1000])[0]
+
+    valid_dataset = datasets.OxfordIIITPet(
+        root=data_dir, split = 'trainval',
+        download=False, transform=valid_transform,
+    )
+    
+#     valid_idx = []
+#     for i in range(len(valid_dataset)):
+#         if valid_dataset.__getitem__(i)[1] in [1, 2, 3]:
+#             valid_idx.append(i)
+#     valid_dataset = torch.utils.data.Subset(valid_dataset, valid_idx)
+    
+#     print(len(train_idx), train_idx)
+#     print(len(valid_idx), valid_idx)
+    
+    
+    # Olesia added to limit size of the datasets
+    #valid_dataset = torch.utils.data.random_split(valid_dataset, [1000, len(valid_dataset)-1000])[0]
+    
+    # idx = (valid_dataset.targets=="Bengal") | (valid_dataset.targets=="Chihuahua")
+    # valid_dataset.targets = valid_dataset.targets[idx]
+    # valid_dataset.data = valid_dataset.data[idx]
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, #sampler=train_sampler,
+        num_workers=num_workers, pin_memory=pin_memory, shuffle=True, # Olesia added shuffle
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, #sampler=valid_sampler,
+        num_workers=num_workers, pin_memory=pin_memory, shuffle=True, # Olesia added shuffle
+    )
+
+    # visualize some images
+    if show_sample:
+        sample_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=9, shuffle=shuffle,
+            num_workers=num_workers, pin_memory=pin_memory,
+        )
+        # Get a batch of training data
+        inputs, classes = next(iter(sample_loader))
+        
+        # Make a grid from batch
+        out = torchvision.utils.make_grid(inputs, nrow=3)
+        plt.figure(figsize = (10,10))
+        imshow(out)
+
+    return (train_loader, valid_loader)
+
+
+
+def get_test_loader(data_dir,
+                    batch_size,
+                    shuffle=True,
+                    num_workers=4,
+                    pin_memory=False):
+    """
+    Utility function for loading and returning a multi-process
+    test iterator over the CIFAR-10 dataset.
+
+    If using CUDA, num_workers should be set to 1 and pin_memory to True.
+
+    Params
+    ------
+    - data_dir: path directory to the dataset.
+    - batch_size: how many samples per batch to load.
+    - shuffle: whether to shuffle the dataset after every epoch.
+    - num_workers: number of subprocesses to use when loading the dataset.
+    - pin_memory: whether to copy tensors into CUDA pinned memory. Set it to
+      True if using GPU.
+
+    Returns
+    -------
+    - data_loader: test set iterator.
+    """
+    normalize = T.Normalize(mean,std)
+
+    # define transform
+    transform = T.Compose([
+        T.Resize((256,256)),
+        T.CenterCrop((224,224)),
+        T.ToTensor(),
+        normalize,
+    ])
+
+    dataset = datasets.OxfordIIITPet(
+        root=data_dir, split = 'test',
+        download=True, transform=transform,
+    )
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle,
+        num_workers=num_workers, pin_memory=pin_memory,
+    )
+
+    return data_loader
+
+
+
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
